2a0a93185b8b6c78b3c0f621da6e18720abaca44
Aleksey <pohodnyaa@gmail.com>
2017-07-04 11:17:45 +0700

    Add support of all icon sets from react-native-vector-icons

--- build/styles.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components/native'
-import values from './values'
-export const Bar = styled.View`
-  backgroundColor: ${props => props.barColor}
-  height: ${values.barHeight}
-`
