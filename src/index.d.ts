92d3f1536d193022c7c428a61f3bf355d2338747
Kyle Roach <kroach.work@gmail.com>
2017-12-26 07:39:32 -0400

    docs(readme, typings): Add text style

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,7 +3,8 @@
 // Definitions by: Kyle Roach <https://github.com/iRoachie>
 // TypeScript Version: 3.1
 
-import * as React from 'react'
+import * as React from 'react';
+import { StyleProp, TextStyle } from 'react-native';
 
 interface TabsProps {
   /**
@@ -11,54 +12,59 @@ interface TabsProps {
    *
    * Default is 0
    */
-  selectedIndex?: number
+  selectedIndex?: number;
 
   /**
    * Color of the tab bar
    *
    * Default is #13897b
    */
-  barColor?: string
+  barColor?: string;
 
   /**
    * Color of the text for the selected tab
    *
    * Default is #fff
    */
-  activeTextColor?: string
+  activeTextColor?: string;
 
   /**
    * Color of the indicator
    *
    * Default is #fff
    */
-  indicatorColor?: string
+  indicatorColor?: string;
 
   /**
    * Color of the text for inactive tabs
    *
    * Default is rgba(255, 255, 255, 0.7)
    */
-  inactiveTextColor?: string
+  inactiveTextColor?: string;
 
   /**
    * Option between having fixed tabs or scrollable tabs
    *
    * Default is false
    */
-  scrollable?: boolean
+  scrollable?: boolean;
 
   /**
    * The titles for the individual tabs
    */
-  items: string[]
+  items: string[];
+
+  /**
+   * Optional text style to pass to tab titles
+   */
+  textStyle?: StyleProp<TextStyle>;
 
   /**
    * Handler that's emitted every time the user presses a tab.
    * You can use this to change the selected index
    * @param index
    */
-  onChange(index: number): void
+  onChange(index: number): void;
 }
 
 export default class MaterialTabs extends React.Component<TabsProps, null> {}
